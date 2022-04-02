2a804e4ce533850f92ff93e4a241486211062aba
Zain Ul Haq <zainulhaq904@gmail.com>
2022-04-03 00:07:01 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Heroku-Web
+# ML-House-Price-Prediction
+Deploy ML model to predict USA house price on Heroku
---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+# In[1]:
+from flask import Flask, request, jsonify, render_template
+import pickle 
+import numpy as np
+import sklearn
+# In[2]:
+#change the save path location for model.bin depends on your computer
+#save_path = r"C:\Users\nguye\Desktop\Data Science Stuff\Projects\Python\Flask\model_file\model.bin"
+# # Deployment of Flask
+# In[3]:
+app = Flask(__name__)
+with open('model_files/model.bin','rb') as f:
+    model = pickle.load(f)
+# In[4]:
+@app.route("/")
+def home():
+    return render_template("index.html")
+# In[5]:
+@app.route('/',methods=['POST'])
+def predict():
+    int_features = [ int (x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0],2)
+    return render_template('index.html', prediction_text='House Price should be $ {:,}'.format(output))
+# In[ ]:
+if __name__ == '__main__':
+    app.run()
+
+
+
+
